3d20928f3bd9b6b3b1b5d0ec4837bc2ce6d6f56a
unknown <recfrag@gmail.com>
2019-08-18 17:23:17 +0300

    close button fix

--- src/components/PopUpPic/PopUpPic.js
+++ src/components/PopUpPic/PopUpPic.js
@@ -5,10 +5,10 @@ export default function PopUpPic({ title, originalUrl, description, closePopup }
   return (
     <div className="popup">
       <div className="popup_inner">
+        <span className="close" onClick={closePopup}>ЗАКРЫТЬ</span>
         <h2>{title}</h2>
         <img src={originalUrl} alt="cover" id="image" />
         <div>{description}</div>
-        <span className="close" onClick={closePopup}>ЗАКРЫТЬ</span>
       </div>
     </div>
   )
