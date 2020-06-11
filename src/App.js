fb68685c98fcfdc9bb316cdf7136b0da0c94d6a5
Dmitry <recfrag@gmail.com>
2020-06-11 21:21:53 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from "react";
-import "./App.css";
 import Header from "./components/Header/Header";
 import BooksContainer from "./components/BooksContainer/BooksContainer";
 import { books } from "./common/data";
